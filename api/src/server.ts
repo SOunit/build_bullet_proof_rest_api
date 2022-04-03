aa381e9d759f50164af78652fa52d0e9692ff16f
SOunit <workbookwork7@gmail.com>
2022-04-03 13:17:18 -0700

    add get request handler

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,1 +1,12 @@
-console.log("hello");
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  return res.send("hello");
+});
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
